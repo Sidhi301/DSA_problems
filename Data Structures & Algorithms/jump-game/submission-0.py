df7b62ed93c9d5ab4162d49a9eed48547ae077df
Sidhi301 <163748579+Sidhi301@users.noreply.github.com>
2026-06-22 14:33:27 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        maxJump=0
+        for i in range(len(nums)):
+            if i>maxJump:
+                return False
+            maxJump=max(maxJump,i+nums[i])
+
+            if maxJump >=len(nums):
+                return True
+        return True
+        
+
+        
